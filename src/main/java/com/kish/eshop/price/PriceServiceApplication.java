abe4908d1484da2db8b2671435a56777775a0b54
wodehai_glb <goodMorning_glb@163.com>
2019-03-01 14:24:15 +0800

    first commit

---
+++ src/main/java/com/kish/eshop/price/PriceServiceApplication.java
@@ -0,0 +1,28 @@
+package com.kish.eshop.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PriceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceServiceApplication.class, args); 
+	}
+	
+	@Bean
+	public JedisPool jedis() {
+	    JedisPoolConfig config = new JedisPoolConfig();
+	    config.setMaxTotal(100);
+	    config.setMaxIdle(5);
+	    config.setMaxWaitMillis(1000*10);
+	    config.setTestOnBorrow(true);
+	    return new JedisPool(config, "192.168.1.40", 1111);
+	}
+}
---
+++ src/main/java/com/kish/eshop/price/service/ProductPriceService.java
@@ -0,0 +1,15 @@
+package com.kish.eshop.price.service;
+
+import com.kish.eshop.price.model.ProductPrice;
+
+public interface ProductPriceService {
+	
+	public void add(ProductPrice productPrice);
+	
+	public void update(ProductPrice productPrice);
+	
+	public void delete(Long id);
+	
+	public ProductPrice findById(Long id);
+	
+}
